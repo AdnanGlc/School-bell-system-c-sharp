fd7ded4a1b1fa9961fed87816509912f91ae0118
Adnan galijašević <97401156+AdnanGlc@users.noreply.github.com>
2022-06-07 18:19:11 +0200

    Add files via upload

---
+++ Skolsko_Zvono/Skolsko_Zvono/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Skolsko_Zvono
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Vrijeme_label = new System.Windows.Forms.Label();
+            this.Cas_comboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Smjena_comboBox = new System.Windows.Forms.ComboBox();
+            this.PrvaSmjena_TimePicker = new System.Windows.Forms.DateTimePicker();
+            this.DrugaSmjena_TimePicker = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SljedeceZvono_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(199, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(199, 228);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(213, 308);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(419, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(221, 419);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Podesite dužinu trajanja časa:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Vrijeme_label
+            // 
+            this.Vrijeme_label.AutoSize = true;
+            this.Vrijeme_label.Location = new System.Drawing.Point(594, 9);
+            this.Vrijeme_label.Name = "Vrijeme_label";
+            this.Vrijeme_label.Size = new System.Drawing.Size(46, 17);
+            this.Vrijeme_label.TabIndex = 7;
+            this.Vrijeme_label.Text = "label2";
+            // 
+            // Cas_comboBox
+            // 
+            this.Cas_comboBox.FormattingEnabled = true;
+            this.Cas_comboBox.Items.AddRange(new object[] {
+            "Svi časovi",
+            "Prvi čas",
+            "Drugi čas",
+            "Treći čas",
+            "Četvrti čas",
+            "Peti čas",
+            "Šesti čas",
+            "Sedmi čas"});
+            this.Cas_comboBox.Location = new System.Drawing.Point(16, 376);
+            this.Cas_comboBox.Name = "Cas_comboBox";
+            this.Cas_comboBox.Size = new System.Drawing.Size(121, 24);
+            this.Cas_comboBox.TabIndex = 8;
+            this.Cas_comboBox.SelectedIndexChanged += new System.EventHandler(this.Cas_comboBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Početak prve smjene:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Početak druge smjene:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Trajanja malog odmora:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Trajanja velikog odmora:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 477);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(283, 35);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Ažuriraj raspored zvonjenja";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Smjena_comboBox
+            // 
+            this.Smjena_comboBox.FormattingEnabled = true;
+            this.Smjena_comboBox.Items.AddRange(new object[] {
+            "Prva Smjena",
+            "Druga smjena"});
+            this.Smjena_comboBox.Location = new System.Drawing.Point(16, 345);
+            this.Smjena_comboBox.Name = "Smjena_comboBox";
+            this.Smjena_comboBox.Size = new System.Drawing.Size(121, 24);
+            this.Smjena_comboBox.TabIndex = 15;
+            this.Smjena_comboBox.SelectedIndexChanged += new System.EventHandler(this.Smjena_comboBox_SelectedIndexChanged);
+            // 
+            // PrvaSmjena_TimePicker
+            // 
+            this.PrvaSmjena_TimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.PrvaSmjena_TimePicker.Location = new System.Drawing.Point(199, 85);
+            this.PrvaSmjena_TimePicker.Name = "PrvaSmjena_TimePicker";
+            this.PrvaSmjena_TimePicker.ShowUpDown = true;
+            this.PrvaSmjena_TimePicker.Size = new System.Drawing.Size(114, 22);
+            this.PrvaSmjena_TimePicker.TabIndex = 16;
+            // 
+            // DrugaSmjena_TimePicker
+            // 
+            this.DrugaSmjena_TimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.DrugaSmjena_TimePicker.Location = new System.Drawing.Point(199, 135);
+            this.DrugaSmjena_TimePicker.Name = "DrugaSmjena_TimePicker";
+            this.DrugaSmjena_TimePicker.ShowUpDown = true;
+            this.DrugaSmjena_TimePicker.Size = new System.Drawing.Size(114, 22);
+            this.DrugaSmjena_TimePicker.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 35);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Sačuvaj promjene";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 426);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 35);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Sačuvaj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // SljedeceZvono_label
+            // 
+            this.SljedeceZvono_label.AutoSize = true;
+            this.SljedeceZvono_label.Location = new System.Drawing.Point(647, 443);
+            this.SljedeceZvono_label.Name = "SljedeceZvono_label";
+            this.SljedeceZvono_label.Size = new System.Drawing.Size(46, 17);
+            this.SljedeceZvono_label.TabIndex = 20;
+            this.SljedeceZvono_label.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 559);
+            this.Controls.Add(this.SljedeceZvono_label);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DrugaSmjena_TimePicker);
+            this.Controls.Add(this.PrvaSmjena_TimePicker);
+            this.Controls.Add(this.Smjena_comboBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Cas_comboBox);
+            this.Controls.Add(this.Vrijeme_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Školsko Zvono";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Vrijeme_label;
+        private System.Windows.Forms.ComboBox Cas_comboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox Smjena_comboBox;
+        private System.Windows.Forms.DateTimePicker PrvaSmjena_TimePicker;
+        private System.Windows.Forms.DateTimePicker DrugaSmjena_TimePicker;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label SljedeceZvono_label;
+    }
+}
+
---
+++ Skolsko_Zvono/Skolsko_Zvono/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+namespace Skolsko_Zvono
+{
+    public partial class Form1 : Form
+    {
+        string FilePath = @"C:\Users\User\Documents\Visual Studio 2013\Projects\Skolsko_Zvono\RasporedZvonjenja.txt";
+        int[,] TrajanjeCasa = new int[2, 7];
+        int MaliOdmor = 5;
+        int VelikiOdmor = 20;
+        DateTime PrvaSmjena = new DateTime();
+        DateTime DrugaSmjena = new DateTime();
+        DateTime Zvono = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0, 0);
+        DateTime[] Raspored = new DateTime[28];
+        bool PlaySound = true;
+
+        DateTime PostaviPodatke(int sati, int minute)
+        {
+            int dani = 0;
+            while (minute >= 60)
+            {
+                minute -= 60;
+                sati++;
+            }
+            while (sati >= 24)
+            {
+                sati -= 24;
+                dani++;
+            }
+            DateTime dt = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + dani, sati, minute, 0, 0);
+            return dt;
+        }
+        void PostaviZvono()
+        {
+            int index = 0;
+            Zvono = Raspored[index++];
+            while (true)
+            {
+                if (Zvono.Hour == DateTime.Now.Hour)
+                {
+                    if (Zvono.Minute >= DateTime.Now.Minute)
+                    {
+                        break;
+                    }
+                }
+                if (Zvono.Hour > DateTime.Now.Hour) break;
+                Zvono = Raspored[index++];
+                if (index == 28) break;
+            }
+            if (index == 28) Zvono = PrvaSmjena;
+        }
+
+        void IspisiRaspored()
+        {
+            int index = 0;
+            richTextBox1.Text = " \n\n\n";
+            richTextBox1.Text += " Pocetak prve smjene:  " + PrvaSmjena.ToShortTimeString() + "\n";
+            Zvono = PrvaSmjena;
+            Raspored[index++] = Zvono;
+            for (int i = 0; i < 3; i++)
+            {
+                richTextBox1.Text += (i + 1) + ". cas:  " + Zvono.ToShortTimeString();
+                Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + TrajanjeCasa[0, i]);
+                Raspored[index++] = Zvono;
+                richTextBox1.Text += " - " + Zvono.ToShortTimeString() + "\n";
+                Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + MaliOdmor);
+                if (i != 2) Raspored[index++] = Zvono;
+            }
+            Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + VelikiOdmor - MaliOdmor);
+            Raspored[index++] = Zvono;
+            richTextBox1.Text += "Veliki odmor do: " + Zvono.ToShortTimeString() + "\n";
+            for (int i = 3; i < 7; i++)
+            {
+                richTextBox1.Text += (i + 1) + ". cas:  " + Zvono.ToShortTimeString();
+                Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + TrajanjeCasa[0, i]);
+                Raspored[index++] = Zvono;
+                richTextBox1.Text += " - " + Zvono.ToShortTimeString() + "\n";
+                Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + MaliOdmor);
+                if (i != 6) Raspored[index++] = Zvono;//nema potrebe dodavati mali odmor nakon sedmog casa
+            }
+            //druga smjena
+            richTextBox1.Text += "\n\n\n";
+            richTextBox1.Text += " Pocetak druge smjene:  " + DrugaSmjena.ToShortTimeString() + "\n";
+            Zvono = DrugaSmjena;
+            Raspored[index++] = Zvono;
+            for (int i = 0; i < 3; i++)
+            {
+                richTextBox1.Text += (i + 1) + ". cas:  " + Zvono.ToShortTimeString();
+                Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + TrajanjeCasa[1, i]);
+                Raspored[index++] = Zvono;
+                richTextBox1.Text += " - " + Zvono.ToShortTimeString() + "\n";
+                Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + MaliOdmor);
+                if (i != 2) Raspored[index++] = Zvono;
+            }
+            Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + VelikiOdmor - MaliOdmor);
+            Raspored[index++] = Zvono;
+            richTextBox1.Text += "Veliki odmor do: " + Zvono.ToShortTimeString() + "\n";
+            for (int i = 3; i < 7; i++)
+            {
+                richTextBox1.Text += (i + 1) + ". cas:  " + Zvono.ToShortTimeString();
+                Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + TrajanjeCasa[1, i]);
+                Raspored[index++] = Zvono;
+                richTextBox1.Text += " - " + Zvono.ToShortTimeString() + "\n";
+                Zvono = PostaviPodatke(Zvono.Hour, Zvono.Minute + MaliOdmor);
+                if (i != 6) Raspored[index++] = Zvono;
+            }
+            for(int i=0;i<28;i++)
+            {
+             for(int j=i;j<28;j++)
+                {
+                    if(Raspored[i].Hour >Raspored[j].Hour || (Raspored[i].Hour==Raspored[j].Hour && Raspored[i].Minute>Raspored[j].Minute))
+                    {
+                        DateTime temp = Raspored[i];
+                        Raspored[i] = Raspored[j];
+                        Raspored[j] = temp;
+                    }
+                }
+            }
+
+                PostaviZvono();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //inicijalizacija
+            string[] lines = File.ReadAllLines(FilePath);    //citanje svih linija koda
+            int Index = lines[0].IndexOf(',');               //posto moze biti ejdnoscifreni i dvocifreni broj moramo naci indeks , kako bi ispravno podijelili stringove
+            PrvaSmjena = PostaviPodatke(Convert.ToInt32(lines[0].Substring(0, Index)), Convert.ToInt32(lines[0].Substring(Index + 1)));//postavimo vrijendnost sati i minitua pprve smjene prema dokumentu
+            Index = lines[1].IndexOf(',');                    //isti proces ponovimo za drugu smjenu
+            DrugaSmjena = PostaviPodatke(Convert.ToInt32(lines[1].Substring(0, Index)), Convert.ToInt32(lines[1].Substring(Index + 1)));
+            for (int i = 0; i < 2; i++)                       //prema dokumentu zapamtimo u matricu koliko časovi traju 
+                for (int j = 0; j < 7; j++)
+                    TrajanjeCasa[i, j] = Convert.ToInt32(lines[4 + 7 * i + j]);
+            MaliOdmor = Convert.ToInt32(lines[2]);            //duzina malog odmora je zapisana u 3 liniji teksta
+            VelikiOdmor = Convert.ToInt32(lines[3]);          //duzina malog odmora je zapisana u 3 liniji teksta
+            Cas_comboBox.SelectedIndex = 0;
+            Smjena_comboBox.SelectedIndex = 0;
+            textBox1.Text = MaliOdmor.ToString();
+            textBox2.Text = VelikiOdmor.ToString();
+            PrvaSmjena_TimePicker.Value = PrvaSmjena;
+            DrugaSmjena_TimePicker.Value = DrugaSmjena;
+            timer1.Start();
+            IspisiRaspored();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IspisiRaspored();
+        }
+
+        private void Smjena_comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Cas_comboBox.SelectedIndex != 0)
+                textBox3.Text = TrajanjeCasa[Smjena_comboBox.SelectedIndex, Cas_comboBox.SelectedIndex - 1].ToString();
+        }
+
+        private void Cas_comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Cas_comboBox.SelectedIndex != 0)
+                textBox3.Text = TrajanjeCasa[Smjena_comboBox.SelectedIndex, Cas_comboBox.SelectedIndex - 1].ToString();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int Minute = new int();
+            try
+            {
+                if (textBox3.Text != "") Minute = Convert.ToInt32(textBox3.Text);
+                MaliOdmor = Convert.ToInt32(textBox1.Text);
+                VelikiOdmor = Convert.ToInt32(textBox2.Text);
+                PrvaSmjena = PrvaSmjena_TimePicker.Value;
+                DrugaSmjena = DrugaSmjena_TimePicker.Value;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            if (textBox3.Text == "") return;
+            if (Minute <= 0 || Minute > 60)
+            {
+                MessageBox.Show("Unesite trajanje časa između 1-60 minuta");
+                return;
+            }
+            if (Cas_comboBox.SelectedIndex == 0)
+            {
+                for (int i = 0; i < 7; i++)
+                    TrajanjeCasa[Smjena_comboBox.SelectedIndex, i] = Minute;
+            }
+            else TrajanjeCasa[Smjena_comboBox.SelectedIndex, Cas_comboBox.SelectedIndex - 1] = Minute;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Vrijeme_label.Text = DateTime.Now.ToLongTimeString();
+            TimeSpan ts = Zvono - DateTime.Now;
+            int minute = Convert.ToInt32(ts.TotalMinutes);
+            SljedeceZvono_label.Text = "Zvoni za: " + minute + " min";
+            if (DateTime.Now.Hour == Zvono.Hour && DateTime.Now.Minute == Zvono.Minute && PlaySound)
+            {
+                SoundPlayer sp = new SoundPlayer(@"C:\Users\User\Documents\Visual Studio 2013\Projects\Skolsko_Zvono\Zvono.wav");
+                sp.Play();
+                PlaySound = false;
+            }
+            if (DateTime.Now.Minute != Zvono.Minute && !PlaySound)
+            {
+                PlaySound = true;
+                PostaviZvono();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines(FilePath);
+            lines[0] = Convert.ToInt32(PrvaSmjena.Hour) + "," + Convert.ToInt32(PrvaSmjena.Minute);
+            lines[1] = Convert.ToInt32(DrugaSmjena.Hour) + "," + Convert.ToInt32(DrugaSmjena.Minute);
+            lines[2] = MaliOdmor.ToString();
+            lines[3] = VelikiOdmor.ToString();
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 7; j++)
+                    lines[4 + i * 7 + j] = TrajanjeCasa[i, j].ToString();
+            File.WriteAllLines(FilePath, lines);
+            MessageBox.Show("Prommjene su trajno sačuvane");
+        }
+    }
+}
